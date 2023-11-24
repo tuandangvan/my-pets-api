e4bcf3fe52177f0d806ef00f7b8baaf00bda2d3b
username <tuandangvann@gmail.com>
2023-11-25 01:52:23 +0700

    add socket.io

--- src/socketServer.js
+++ src/socketServer.js
@@ -45,9 +45,8 @@
 
 import { Server } from "socket.io";
 const socketServer = (server) => {
-  const io = new Server(server, { cors: "https://found-and-adoption-pet-api-be.vercel.app" });
+  const io = new Server(server, { cors: "https://localhost:8050" });
   console.log("Running socket!");
-  
   let onlineUsers = [];
   // Lắng nghe kết nối từ các client
   io.on("connection", (socket) => {
