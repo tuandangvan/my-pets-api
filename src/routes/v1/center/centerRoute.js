fec4f8e3d631d77ef0094a9eb709cc3d23d1958e
VanTuan <tuandangvann@gmail.com>
2024-07-13 19:03:03 +0700

    get profile

--- src/routes/v1/center/centerRoute.js
+++ src/routes/v1/center/centerRoute.js
@@ -9,8 +9,7 @@ const router = express.Router();
 router.post("/:accountId", centerController.createInfoForCenter);
 router.put("/:centerId", authencation, authorizationMiddelware.permission(PermissionRoles.onlyCenter), 
 centerController.updateCenter);
-router.get("/:centerId", authencation, authorizationMiddelware.permission(PermissionRoles.All), 
-centerController.getCenter);
+router.get("/:centerId", centerController.getCenter);
 
 router.put("/:centerId/status", authencation, authorizationMiddelware.permission(PermissionRoles.onlyUser),
 centerController.changStatusAccount);
